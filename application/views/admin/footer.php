ac44ddbacca6687baee783c4372df1c10b18be60
rendysatriananto <rendysatriananto@gmail.com>
2022-03-17 13:11:39 +0700

    initial commit

---
+++ application/views/admin/footer.php
@@ -0,0 +1,11 @@
+</div>
+<script type="text/javascript">
+	$(document).ready(function(){
+		$("#table-datatable").dataTable();
+	});
+	$('.alert-message').alert().delay(3000).slideUp('slow');
+
+	var a = 1;
+</script>
+</body>
+</html>
